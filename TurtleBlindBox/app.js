2a04b078dade1ec174148b75ec09ee345c07b1f1
Bear <SzuYuan521@gmail.com>
2024-08-19 20:53:16 +0800

    [+] 新增音效, 播報素材, favicon, 新增訂單, 規則, 分數計算及樣式
    1. 新增sounds folder及音效
    2. 新增favicon.ico
    3. 新增訂單數量orderNumber的選擇介面跟按鈕監聽還有流程
    4. 新增幸運色烏龜的選擇介面跟監聽還有流程, 選完幸運色後發盲盒
    5. 新增turtleCount[]儲存已得的烏龜數量, 在每次revealBlindBox時增加
    6. 新增cells[]儲存格子, 防止重複調用造成的資源消耗
    7. 新增dealingBlindBox(cellId), 照格子順序每隔100ms發盲盒, 只發空格子, 發完一個更新一次orderNumber
    8. 新增revealBlindBox()處理拆盲盒, 正在播報時不給拆, 拆盲盒時會有個開啟音效, 如果拆出的烏龜符合幸運色, 則當下立刻多一包, 如果9格全拆完就進行特殊牌型分數判斷, 判斷完沒有剩餘盲盒就遊戲結束, 不然就繼續發
    9. 新增getRandomTurtle(), 骰一個隨機色烏龜
    10. 新增cleanCell(cellId), 重設該格子的狀態為none並將圖片設為hidden
    11. 新增judgmentAllDifferent()判斷全家福, 並回傳一個boolean值
    12. 新增judgmentLine()判斷是否有三個同樣顏色連成一線, 額外建一個colors[]儲存每格烏龜顏色, lines[]直接定義所有連線的位置, 利用解構賦值檢查連線, 並記錄第一個符合的, 消除此連線, 並回傳一個boolean值
    13. 新增judgmentPair()判斷是否有兩個相同顏色的烏龜, 消除第一個符合的對子, 並回傳一個boolean值
    14. 新增judgmentEmpty()判斷是否全部格子都清空沒烏龜了
    15. 新增judgmentScore(), 照11~14的順序判斷, 每次判斷如果有消除則播報並間隔2秒再判斷下一個
    16. 新增reportObj儲存對應牌型的播報文字, 音效, 給予盲盒數
    17. 新增report()用以遊戲戰況播報處理
    18. 新增gameEnd(), 先暫時印出最後得到的烏龜數量
    19. 新增放大縮小動畫跟放大動畫css以及js處理邏輯
    20. 新增delayFunction用來一個個發盲盒, 每個間隔100ms
    21. 新增delay()用於延時的輔助函數, 返回一個在指定毫秒(ms)後解析的Promise物件
    22. 移除開始遊戲按鈕改成訂單選擇代替

--- TurtleBlindBox/app.js
+++ TurtleBlindBox/app.js
@@ -3,10 +3,40 @@ document.addEventListener("DOMContentLoaded", () => {
   const height = 3;
 
   const gameContainer = document.getElementById("gameContainer");
-  const startButton = document.getElementById("startButton");
+  const orderSelection = document.getElementById("order-selection");
+  const reportDiv = document.getElementById("report");
 
-  // 暫建一個開始遊戲按鈕
-  startButton.addEventListener("click", startGame);
+  // 準備音效
+  const clickSound = new Audio("sounds/click-sound.mp3");
+  const openSound = new Audio("sounds/open-sound.mp3");
+
+  // 按鈕點擊添加播放音效
+  orderSelection.querySelectorAll("button").forEach((button) => {
+    button.addEventListener("click", () => {
+      clickSound.play();
+    });
+  });
+
+  // 訂單數量選擇的監聽
+  orderSelection.addEventListener("click", orderSelect);
+
+  // 盲盒圖片名稱
+  const turtleBlindBox = "turtle-blind-box";
+
+  // (訂單數量)拆幾包盲盒
+  let orderNumber = 0;
+
+  // 選擇訂單(數量)
+  function orderSelect(event) {
+    if (event.target.tagName !== "BUTTON") return;
+    orderNumber = event.target.getAttribute("data-number");
+    startGame();
+  }
+
+  let isReporting = false;
+
+  // 幸運色烏龜
+  let luckyTurtle = "";
 
   // 烏龜顏色array, 同時也是初始化的排列順序
   const turtleColors = [
@@ -21,26 +51,46 @@ document.addEventListener("DOMContentLoaded", () => {
     "turquoise-turtle",
   ];
 
+  // 儲存已得烏龜數量
+  const turtleCount = {};
+  turtleColors.forEach((color) => {
+    turtleCount[color] = 0;
+  });
+
+  // 儲存格子的array
+  const cells = [];
+
   function startGame() {
+    orderSelection.style.display = "none";
+
     gameContainer.innerHTML = "";
+    gameContainer.style.display = "grid";
+
+    const text = document.getElementById("tip-text");
+    text.querySelector("p").textContent = "請選擇幸運色";
 
     // 創建遊戲面版
     const fragment = document.createDocumentFragment();
     for (let i = 0; i < width * height; i++) {
-      const cell = createCell(i, turtleColors[i]);
+      const cell = createCell(i, turtleColors[i], "none");
+      cells.push(cell);
       fragment.appendChild(cell);
     }
+
     gameContainer.appendChild(fragment);
+
+    gameContainer.addEventListener("click", chooseLuckyTurtle);
   }
 
-  // 創建格子, id: index, color: 烏龜顏色
-  function createCell(id, color) {
+  // 創建格子, id: index, color: 烏龜顏色, status:blindBox/turtle/none => 盲盒/烏龜/沒東西
+  function createCell(id, color, status) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     cell.setAttribute("data-id", id);
     cell.setAttribute("data-color", color);
+    cell.setAttribute("data-status", status);
 
-    // 烏龜圖片初始化, 可以選擇幸運色龜龜
+    // 烏龜圖片初始化, 可以選擇幸運色烏龜
     const imgElement = document.createElement("img");
     imgElement.src = `img/${color}.png`;
     imgElement.alt = `${color} turtle`;
@@ -54,5 +104,425 @@ document.addEventListener("DOMContentLoaded", () => {
     cell.appendChild(indexElement);
 
     return cell;
+  }
+
+  // 選擇幸運色烏龜後的處理(遊戲正式開始))
+  function chooseLuckyTurtle(event) {
+    const cell = event.target.closest(".cell");
+
+    if (!cell) return;
+
+    // 幸運色設定
+    const color = cell.getAttribute("data-color");
+    luckyTurtle = color;
+
+    // 幸運色烏龜圖片路徑設定
+    const luckyTurtleImg = document.getElementById("lucky-turtle-img");
+    luckyTurtleImg.src = `img/${luckyTurtle}.png`;
+
+    const text = document.getElementById("tip-text");
+    text.querySelector("p").textContent = "請打開所有包裝";
+    //  text.style.visibility = "hidden";
+
+    // 顯示規則跟幸運色
+    const description = document.getElementById("rule-text");
+    description.style.visibility = "visible";
+
+    gameContainer.removeEventListener("click", chooseLuckyTurtle);
+
+    // 設定剩餘盲盒數字
+    const countText = document.getElementById("count-text").querySelector("p");
+    countText.textContent = `還有${orderNumber}包`;
+
+    for (let i = 0; i < width * height; i++) {
+      cleanCell(i);
+    }
+
+    // 依序發盲盒
+    delayFunction(0, dealingBlindBox);
+  }
+
+  // 發盲盒
+  function dealingBlindBox(cellId) {
+    // console.log("orderNumber = " + orderNumber);
+    if (orderNumber === 0) return;
+    const cell = cells[cellId];
+
+    // 不是空格子不發
+    if (cell.getAttribute("data-status") !== "none") return;
+
+    cell.setAttribute("data-status", "blindBox");
+    cell.setAttribute("data-color", turtleBlindBox);
+
+    const imgElement = cell.querySelector(".turtle-image");
+    imgElement.style.visibility = "visible";
+    imgElement.src = `img/${turtleBlindBox}.png`;
+    imgElement.alt = turtleBlindBox;
+
+    cell.addEventListener("click", revealBlindBox);
+    cell.addEventListener("click", revealBlindBox);
+    imgElement.addEventListener("click", zoomImage);
+
+    orderNumber--;
+
+    // 設定剩餘盲盒數字
+    const countText = document.getElementById("count-text").querySelector("p");
+    countText.textContent = `還有${orderNumber}包`;
+
+    openSound.play();
+  }
+
+  // 拆盲盒
+  async function revealBlindBox(event) {
+    // 正在播報時不給拆
+    if (isReporting) {
+      return;
+    }
+
+    const cell = event.target.closest(".cell");
+    if (!cell) return;
+
+    if (cell.getAttribute("data-status") !== "blindBox") return;
+
+    openSound.play();
+
+    cell.setAttribute("data-status", "turtle");
+
+    const color = getRandomTurtle();
+    cell.setAttribute("data-color", color);
+    // 統計顏色數量
+    turtleCount[color]++;
+
+    const imgElement = cell.querySelector(".turtle-image");
+    imgElement.src = `img/${color}.png`;
+    imgElement.alt = `${color} turtle`;
+
+    cell.removeEventListener("click", revealBlindBox);
+
+    // 許願色烏龜處理
+    if (color == luckyTurtle) {
+      orderNumber += reportObj["WISH"].bonus;
+      report("WISH");
+    }
+
+    // 檢查是不是9格全拆完, 如果不是則return
+    for (let i = 0; i < width * height; i++) {
+      if (
+        cells[i].getAttribute("data-status") !== "turtle" &&
+        cells[i].getAttribute("data-status") !== "none"
+      ) {
+        return;
+      }
+    }
+
+    await judgmentScore();
+
+    // 如果9格全拆完, 沒有剩餘盲盒就遊戲結束
+    if (orderNumber == 0) {
+      gameEnd();
+      return;
+    }
+
+    // 依序發盲盒
+    delayFunction(0, dealingBlindBox);
+  }
+
+  // 隨機色烏龜
+  function getRandomTurtle() {
+    const index = Math.floor(Math.random() * turtleColors.length);
+    return turtleColors[index];
+  }
+
+  // 清除格子圖片和狀態
+  function cleanCell(cellId) {
+    const cell = cells[cellId];
+    cell.setAttribute("data-status", "none");
+    cell.setAttribute("data-color", "none");
+    const imgElement = cell.querySelector(".turtle-image");
+    imgElement.style.visibility = "hidden";
+  }
+
+  // 判斷有沒有特殊牌型, 有的話給盲盒
+  /*  許願色+1包(開盲盒時先加)
+      全家福(9個不一樣)+5包
+      連線(三個連線)+5包
+      對碰(兩個同色)+1包
+      清空(9個全消)+5包 */
+  async function judgmentScore() {
+    await delay(1000); // 等待1秒
+
+    // 先判斷全家福, 如果有全家福會全清空, 則不用判斷接下來的
+    const allDifferent = await judgmentAllDifferent();
+    if (allDifferent) {
+      return;
+    }
+
+    // 判斷連線, 預設有連線, 直到判斷沒有連線了才判斷對碰
+    let hasLine = true;
+    while (hasLine) {
+      hasLine = await judgmentLine();
+      // 隔1秒後再判斷下一個條件
+    }
+
+    // 判斷對子, 預設有對子, 直到判斷沒有對子了才判斷清空
+    let hasPair = true;
+    while (hasPair) {
+      hasPair = await judgmentPair();
+    }
+
+    // 判斷有沒有被清空
+    await judgmentEmpty();
+  }
+
+  // 判斷全家福
+  async function judgmentAllDifferent() {
+    let allDifferent = true;
+
+    // 因為cell只有9格, 所以用forEach增加程式碼可讀性
+    cells.forEach((cell, index) => {
+      const color = cell.getAttribute("data-color");
+      // 檢查cell中的顏色是否都不一樣
+      cells.forEach((otherCell, otherIndex) => {
+        if (
+          otherIndex !== index &&
+          otherCell.getAttribute("data-color") === color
+        ) {
+          allDifferent = false;
+        }
+      });
+    });
+
+    if (allDifferent) {
+      orderNumber += reportObj["ALL_DIFFERENT"].bonus;
+      report("ALL_DIFFERENT");
+      for (let i = 0; i < cells.length; i++) {
+        // 清空該cell
+        cleanCell(i);
+      }
+      await delay(2000); // 等待2秒
+      await judgmentEmpty();
+    }
+
+    return allDifferent;
+  }
+
+  // 判斷是否有三個連成一條線
+  async function judgmentLine() {
+    let hasLine = false;
+
+    // 獲取每個格子的顏色
+    const colors = cells.map((cell) => {
+      return cell.getAttribute("data-status") === "turtle"
+        ? cell.getAttribute("data-color")
+        : "none";
+    });
+
+    // 定義所有可能的連線, 傳入checkThreeInARow()判斷
+    const lines = [
+      [0, 1, 2], // 第一行
+      [3, 4, 5], // 第二行
+      [6, 7, 8], // 第三行
+      [0, 3, 6], // 第一列
+      [1, 4, 7], // 第二列
+      [2, 5, 8], // 第三列
+      [0, 4, 8], // 對角線 (左上到右下)
+      [2, 4, 6], // 對角線 (右上到左下)
+    ];
+
+    // 使用解構賦值, 檢查是否有三個相同顏色的連線, 並記錄第一個index
+    const checkThreeInARow = (index) => {
+      const [a, b, c] = index;
+      // 如果color abc都存在, 而且顏色一樣
+      if (
+        colors[a] &&
+        colors[b] &&
+        colors[c] &&
+        colors[a] === colors[b] &&
+        colors[b] === colors[c] &&
+        colors[a] !== "none"
+      ) {
+        return index; // 返回這三個index
+      }
+      return null; // 沒有找到三連線時返回null
+    };
+
+    for (let i = 0; i < lines.length; i++) {
+      const result = checkThreeInARow(lines[i]);
+      if (result !== null) {
+        orderNumber += reportObj["LINE"].bonus;
+        report("LINE");
+
+        // 清除連線的格子
+        cleanCell(lines[i][0]);
+        cleanCell(lines[i][1]);
+        cleanCell(lines[i][2]);
+
+        hasLine = true;
+      }
+    }
+
+    if (hasLine) {
+      // 隔2秒後再判斷下一個條件
+      await delay(2000);
+    }
+
+    // 沒檢查到連線了, 回傳false
+    return hasLine;
+  }
+
+  // 判斷是否對碰
+  async function judgmentPair() {
+    let hasPair = false;
+
+    for (let index = 0; index < cells.length; index++) {
+      const cell = cells[index];
+      const color = cell.getAttribute("data-color");
+
+      // 如果是該格子不是烏龜就跳過
+      if (cell.getAttribute("data-status") !== "turtle") continue;
+
+      for (let otherIndex = 0; otherIndex < cells.length; otherIndex++) {
+        if (index === otherIndex) continue; // 跳過自己
+
+        const otherCell = cells[otherIndex];
+        if (otherCell.getAttribute("data-color") === color) {
+          orderNumber += 1;
+          report("PAIR");
+          cleanCell(index);
+          cleanCell(otherIndex);
+
+          hasPair = true;
+          break; // 找到一對對碰後就跳出迴圈
+        }
+      }
+
+      if (hasPair) break; // 找到一對對碰後就跳出迴圈
+    }
+
+    if (hasPair) {
+      // 隔2秒後再判斷下一個條件
+      await delay(2000);
+    }
+
+    // 判斷沒有一樣顏色了, 回傳false判斷下一個條件
+    return hasPair;
+  }
+
+  // 判斷是否清空了
+  async function judgmentEmpty() {
+    for (let i = 0; i < cells.length; i++) {
+      if (cells[i].getAttribute("data-color") !== turtleBlindBox) {
+        return;
+      }
+    }
+
+    // 全部cell都沒有有顏色的烏龜, 清空了, +5包
+    orderNumber += 5;
+    report("ALL_CLEAN");
+
+    await delay(2000);
+  }
+
+  // bonus:牌型的bonus盲盒數, report_text:文字播報, audio:音效播放
+  const reportObj = {
+    ALL_DIFFERENT: {
+      bonus: 5,
+      report_text: `全家福+${this.bonus}包`,
+      audio: new Audio("sounds/all-different.mp3"),
+    },
+    LINE: {
+      bonus: 5,
+      audio: new Audio("sounds/line.mp3"),
+      get report_text() {
+        return `連線+${this.bonus}包`;
+      },
+    },
+    PAIR: {
+      bonus: 1,
+      audio: new Audio("sounds/pair.mp3"),
+      get report_text() {
+        return `對碰+${this.bonus}包`;
+      },
+    },
+    ALL_CLEAN: {
+      bonus: 5,
+      audio: new Audio("sounds/all-clean.mp3"),
+      get report_text() {
+        return `全清+${this.bonus}包`;
+      },
+    },
+    WISH: {
+      bonus: 1,
+      audio: new Audio("sounds/wish.mp3"),
+      get report_text() {
+        return `許願色+${this.bonus}包`;
+      },
+    },
+  };
+
+  // 播報
+  function report(reportType) {
+    // 更新剩餘盲盒數字
+    const countText = document.getElementById("count-text").querySelector("p");
+    countText.textContent = `還有${orderNumber}包`;
+
+    const reportData = reportObj[reportType];
+    if (reportData) {
+      console.log(reportData.report_text);
+      reportDiv.textContent = reportData.report_text;
+      if (reportData.audio)
+        reportData.audio.play().catch((error) => {
+          console.error("播放音效失敗: ", error);
+        });
+      zoomReport();
+    } else {
+      console.log(reportData);
+    }
+  }
+
+  function gameEnd() {
+    // 統計烏龜數量
+    Object.entries(turtleCount).forEach(([color, count]) => {
+      console.log(color + " : " + count);
+    });
+  }
+
+  // 動畫事件處理函數
+  function zoomImage(event) {
+    const img = event.target;
+    img.classList.add("zoom-in-out");
+
+    // 0.1秒後移除zoom class
+    setTimeout(() => {
+      img.classList.remove("zoom-in-out");
+    }, 100); // 0.1秒
+  }
+
+  // 處理遊戲播報(顯示動畫)
+  function zoomReport() {
+    isReporting = true;
+    reportDiv.classList.add("zoom-in");
+
+    // 0.1秒後移除zoom class
+    setTimeout(() => {
+      reportDiv.classList.remove("zoom-in");
+      isReporting = false;
+    }, 1500);
+  }
+
+  // 延時遍歷格子, 用來發盲盒用, 一個個發
+  function delayFunction(i, func) {
+    setTimeout(() => {
+      func(i);
+      if (i < width * height - 1) {
+        // 遍歷格子
+        delayFunction(i + 1, func);
+      }
+    }, 100); // 延遲100ms
+  }
+
+  // 延時函數，返回一個在指定毫秒(ms)後解析的Promise物件
+  function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 });
